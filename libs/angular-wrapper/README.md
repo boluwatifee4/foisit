9b07c8f0de3b0c4f52fbfecd368a2f54a3050b17
boluwatifee4 <boluwatifee4@gmail.com>
2024-12-21 11:37:43 +0100

    init and project set up

---
+++ libs/angular-wrapper/README.md
@@ -0,0 +1,7 @@
+# angular-wrapper
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test angular-wrapper` to execute the unit tests.
---
+++ libs/angular-wrapper/src/lib/angular-wrapper/angular-wrapper.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-angular-wrapper',
+  imports: [CommonModule],
+  templateUrl: './angular-wrapper.component.html',
+  styleUrl: './angular-wrapper.component.css',
+})
+export class AngularWrapperComponent {}

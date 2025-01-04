e38296e4955ce46821240c7a993ef095ea12c9e0
boluwatifee4 <boluwatifee4@gmail.com>
2025-01-04 01:00:52 +0100

    first successful demo

--- apps/foisit-ng/src/app/app.component.ts
+++ apps/foisit-ng/src/app/app.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { AssistantService } from '@foisit/angular-wrapper';
+import { CommonModule } from '@angular/common';
 
 @Component({
   imports: [
     NxWelcomeComponent,
     RouterModule,
+    CommonModule
   ],
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -14,8 +16,14 @@ import { AssistantService } from '@foisit/angular-wrapper';
 })
 export class AppComponent {
   title = 'foisit-ng';
+  color = signal('transparent');
 
   constructor(private assistantService: AssistantService) {
-    this.assistantService.addCommand('say hello', () => console.log('Hello!'));
+    this.assistantService.addCommand('background red', () => {
+      console.log('Hello!');
+      this.color.set('red');
+      console.log('Color changed to red', this.color());
+    });
+    this.assistantService.startListening()
   }
 }

3d04149d0adf16d0ff96b088323444b5688e9bd3
boluwatifee4 <boluwatifee4@gmail.com>
2025-01-14 22:42:40 +0100

    Implement AssistantProvider and AssistantActivator in App component with command handling: wip

--- libs/react-wrapper/src/index.ts
+++ libs/react-wrapper/src/index.ts
@@ -1,5 +1,6 @@
 export * from './lib/react-wrapper';
 export * from './lib/components/AssistantProvider';
-// export * from './hooks/useAssistant';
+export * from './lib/components/AssistantActivator';
+export * from './lib/hooks/useAssistant';
 export * from './lib/hooks/useAssistantState';
 export * from './lib/services/AssistantService';
